e4e33814b0a5aa446376d20787feb83ddc211098
king <li.zhengxue@infox.com.cn>
2020-05-31 21:08:32 +0800

    fix

---
+++ swagger-demo/src/main/java/com/example/demo/conf/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+	
+
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+//	}
+//	
+//	private ApiInfo apiInfo() {		
+//		return new ApiInfoBuilder().title("my swagger").version("v1.0")
+//				.contact(new Contact("king", "study.com", "king.zx.li@qq.com"))
+//				.build();
+//	}
+	
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).forCodeGeneration(true).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example")).paths(PathSelectors.any()).build();
+
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("APIs").version("1.0")
+				.contact(new Contact("api", "", "admin@infox.com")).build();
+	}
+
+}
